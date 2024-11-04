42918b03a99f670e5c5fc5cc43ff60618c2a78cb
JanoGomezL <20190857@aloe.ulima.edu.pe>
2024-11-04 16:46:08 -0500

    Menu Pausa

---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPausa : MonoBehaviour
+{
+    public GameObject menuPausa;
+    public Button btnContinue;
+    public Button btnSalir;
+    public GameObject player;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        menuPausa.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        btnContinue.onClick.AddListener(ContinueGame);
+        btnSalir.onClick.AddListener(ExitGame);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.F))
+        {
+            if (isPaused)
+            {
+                ContinueGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        isPaused = true;
+        player.SetActive(false);
+        menuPausa.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void ContinueGame()
+    {
+        isPaused = false;
+        player.SetActive(true);
+        menuPausa.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Saliendo del juego...");
+    }
+}
